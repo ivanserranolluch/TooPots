2815e039ccd0e999010936e65fdbc8b0e326baa8
romOlivo <al341918@uji.es>
2019-04-16 15:01:33 +0200

    Working controller

--- src/main/java/es/uji/ei1027/toopots/TooPotsConfiguration.java
+++ src/main/java/es/uji/ei1027/toopots/TooPotsConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import es.uji.ei1027.toopots.dao.ActividadDao;
+
 @Configuration
 public class TooPotsConfiguration {
 
@@ -14,6 +16,11 @@ public class TooPotsConfiguration {
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
+    }
+	
+	@Bean
+    public ActividadDao myActividadDao() {
+        return new ActividadDao();
     }
     
 }
--- src/main/java/es/uji/ei1027/toopots/controller/ActividadController.java
+++ src/main/java/es/uji/ei1027/toopots/controller/ActividadController.java
@@ -2,18 +2,16 @@ package es.uji.ei1027.toopots.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import es.uji.ei1027.toopots.dao.ActividadDao;
-import es.uji.ei1027.toopots.model.CertificacionesTipoActividad;
 
 @Controller
 @RequestMapping("/actividad")
 public class ActividadController {
 	
+	@SuppressWarnings("unused")
 	private ActividadDao actividadDao;
 	
 	@Autowired
@@ -21,11 +19,9 @@ public class ActividadController {
 		this.actividadDao = actividadDao; 
 	}
 	
-	@RequestMapping(value="/", method=RequestMethod.GET) 
-	public String processAddSubmit(@ModelAttribute("certificacionActividad") CertificacionesTipoActividad certificacionesTipoActividad, BindingResult
-			bindingResult) { 
-	
-		return "redirect:list"; 
+	@RequestMapping(value="", method=RequestMethod.GET) 
+	public String processAddSubmit() {
+		return "actividad/list"; 
 	}
 
 }
