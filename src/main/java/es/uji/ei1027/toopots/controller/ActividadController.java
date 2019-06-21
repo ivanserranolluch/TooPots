c446ae9e91d207f199a4822bd65c11b822f6a7a6
Diego <al342376@uji.es>
2019-06-21 11:52:07 +0200

    monitorActividad añadir y listar funcionando

--- src/main/java/es/uji/ei1027/toopots/controller/ActividadController.java
+++ src/main/java/es/uji/ei1027/toopots/controller/ActividadController.java
@@ -1,29 +1,18 @@
 package es.uji.ei1027.toopots.controller;
 
-import java.sql.Date;
-import java.sql.Time;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import es.uji.ei1027.toopots.dao.ImgActDao;
-import es.uji.ei1027.toopots.model.*;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import es.uji.ei1027.toopots.dao.ActividadDao;
+import es.uji.ei1027.toopots.dao.ImgActDao;
 import es.uji.ei1027.toopots.dao.TipoActividadDao;
+import es.uji.ei1027.toopots.model.Actividad;
 
 @Controller
 @RequestMapping("/actividad")
