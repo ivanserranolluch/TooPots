7bbc7af337904bc976a799d6676cf36764e927ad
Ivan <ivanserranolluch@gmail.com>
2019-06-22 23:32:22 +0200

    funcionalidad de las reservas del cliente, ¡¡¡FALTA EL DISEÑO!!

--- src/main/java/es/uji/ei1027/toopots/controller/ReservaClienteActividadController.java
+++ src/main/java/es/uji/ei1027/toopots/controller/ReservaClienteActividadController.java
@@ -3,24 +3,40 @@ package es.uji.ei1027.toopots.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import es.uji.ei1027.toopots.dao.ClienteDao;
 import es.uji.ei1027.toopots.dao.ReservaClienteActividadDao;
 import es.uji.ei1027.toopots.dao.TipoActividadDao;
+import es.uji.ei1027.toopots.model.Cliente;
+import es.uji.ei1027.toopots.model.ReservaClienteActividad;
+import es.uji.ei1027.toopots.model.User;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/reservaClienteActividad")
 public class ReservaClienteActividadController {
 	
 	private ReservaClienteActividadDao reservaClienteActividadDao;
+	private ClienteDao clienteDao;
 	
 	@Autowired
 	public void setReservaClienteActividadDao(ReservaClienteActividadDao reservaClienteActividadDao) {
 		this.reservaClienteActividadDao = reservaClienteActividadDao; 
+	}
+	@Autowired
+	public void setClienteDao(ClienteDao clienteDao) {
+		this.clienteDao = clienteDao; 
 	}
 	
 	@RequestMapping(value="/list", method=RequestMethod.GET) 
@@ -38,10 +54,41 @@ public class ReservaClienteActividadController {
 		return "reservaClienteActividad/list"; 
 	}
 
+	@RequestMapping(value="/listCliente", method=RequestMethod.GET) 
+	public String listActivitiesCliente(Model model,
+                                 @RequestParam("estado") Optional<Integer> estado, HttpSession session){
+		
+		User user = (User) session.getAttribute("user");
+	    Cliente c=clienteDao.getClienteEmail(user.getEmail());
+	    model.addAttribute("reservasClienteActividad", reservaClienteActividadDao.getReservasCliente(c.getDni()));
+        
+
+		return "reservaClienteActividad/listCliente"; 
+	}
 
 
+	@RequestMapping(value="/cancelar/{id}") 
+	public String deleteReserva(Model
+		model, @PathVariable String id) { 
+			ReservaClienteActividad r=reservaClienteActividadDao.getReservaClienteActividad(id);
+			Date fechaHoy= new Date();
+			Calendar calendar = new GregorianCalendar(r.getFechaActividad().getYear(), r.getFechaActividad().getMonth()-1, r.getFechaActividad().getDay());
+			java.sql.Date fecha = new java.sql.Date(calendar.getTimeInMillis());
+			int diasDiferencia = (int)(fechaHoy.getTime()-fecha.getTime());
+			System.out.println("%%%%%%%% Diferencia : "+diasDiferencia);
+			if (diasDiferencia>1000000000) {
+				reservaClienteActividadDao.deleteReserva(id);
+				return "redirect:../listCliente";
+			}else {
+				return "redirect:../cancelacionCancelacion";
+			}
+		}
 
-
+	@RequestMapping(value="/cancelacionCancelacion", method=RequestMethod.GET)
+	public String listActivitiesAceptadas(Model model) {
+		//model.addAttribute("reservaClienteActividadDao", reservaClienteActividadDao.getReservaClienteActividadAceptadas()); 
+		return "reservaClienteActividad/cancelacionCancelacion"; 
+	}
 	/*@RequestMapping(value="/listAceptadas", method=RequestMethod.GET)
 	public String listActivitiesAceptadas(Model model) {
 		model.addAttribute("reservaClienteActividadDao", reservaClienteActividadDao.getReservaClienteActividadAceptadas()); 
